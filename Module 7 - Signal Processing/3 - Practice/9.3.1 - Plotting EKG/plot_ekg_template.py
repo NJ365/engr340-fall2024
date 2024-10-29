a4213cac9ae7cb5cca1ede4460c9d5e84e1e9094
NJ365 <124932644+NJ365@users.noreply.github.com>
2024-10-29 10:07:06 -0400

    10/29

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -1,18 +1,26 @@
-
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 # import the CSV file using numpy
 path = '../../../data/ekg/mitdb_201.csv'
 
 # load data in matrix from CSV file; skip first two rows
 
-### Your code here ###
+data = np.loadtxt(fname=path, skiprows=2, delimiter=',')
 
 # save each vector as own variable
+ekg_time = data[:,0]
+ekg_MLII = data[:,1]
+ekg_V1 = data[:,2]
 
-### Your code here ###
+dif = np.diff(ekg_MLII)
+square = np.square(dif)
+
+conv = [1,1,1,1,1,1]
+avg = np.convolve(square, conv)
 
 # use matplot lib to generate a single
-
-### Your code here ###
+fig, ax = plt.subplots()
+ax.plot(ekg_time,ekg_V1)
+plt.show()
