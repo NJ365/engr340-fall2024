521f3283f76045005c1cd732f9bc2f8c84fa8167
NJ365 <124932644+NJ365@users.noreply.github.com>
2024-11-04 16:46:52 -0500

    11/4

--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -18,8 +18,18 @@ signal_filepath = path_to_folder + database_name + ".csv"
 Step #1: load data in matrix from CSV file; skip first two rows. Call the data signal.
 """
 
-signal = 0
+signal = np.loadtxt(fname=signal_filepath, skiprows=2, delimiter=',')
+
+og_signal = signal[:,1]
+
+# Show plot of Signal
+plt.plot(og_signal,  label="Original Signal")
+plt.xlabel('Index')
+plt.title("Original Signal")
+plt.show()
+
 ## YOUR CODE HERE ##
+
 
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
@@ -32,19 +42,41 @@ Step 3: Pass data through weighted differentiator
 """
 
 ## YOUR CODE HERE ##
+dif = np.diff(signal[:,1])
 
+# Show plot of diff
+plt.plot(dif,  label="Differential")
+plt.xlabel('Index')
+plt.title("Differential")
+plt.show()
 
 """
 Step 4: Square the results of the previous step
 """
  ## YOUR CODE HERE ##
+square = np.square(dif)
+
+# Show plot of square
+plt.plot(square,  label="Square")
+plt.xlabel('Index')
+plt.title("Squared")
+plt.show()
 
 """
 Step 5: Pass a moving filter over your data
 """
 
 ## YOUR CODE HERE
-# make a plot of the results. Can change the plot() parameter below to show different intermediate signals
-plt.title('Process Signal for ' + database_name)
-plt.plot(signal)
+conv = [1,1,1,1,1,1,1,1]
+avg = np.convolve(square, conv, mode='valid')
+
+# Show plot of moving average
+plt.plot(avg,  label="Moving Average")
+plt.xlabel('Index')
+plt.title("Moving Average")
 plt.show()
+
+
+
+# make a plot of the results. Can change the plot() parameter below to show different intermediate signals
+
